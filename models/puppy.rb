f0b916f3f714789b2dcc3ea13dae380fd890e22b
dagpan <panagiotisntagkinis@gmail.com>
2020-02-20 13:15:02 -0500

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,31 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+
+class Puppy
+    
+    attr_reader :breed  
+    attr_accessor :name, :age
+
+    @@all = []
+    @@pups = {}
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+        @@all << self
+        @@all.each do |p| 
+            if @@pups[p.name]
+            else
+               @@pups[p.name] = [p.breed, p.age]
+            end
+        end
+
+    end
+    def self.all
+        @@all
+    end
+    def self.pups
+        @@pups
+    end
+
+end
